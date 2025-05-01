b04788e86338f7d18cf9de3978c21092982fd3c5
Sebastian S <74125017+sebasinmas@users.noreply.github.com>
2025-05-01 01:48:15 -0400

    Setup inicial del proyecto final

---
+++ backend/src/main/java/com/steampals/SteamPals/SteamPalsApplication.java
@@ -0,0 +1,13 @@
+package com.steampals.SteamPals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamPalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamPalsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/steampals/SteamPals/SteamPalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steampals.SteamPals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamPalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
