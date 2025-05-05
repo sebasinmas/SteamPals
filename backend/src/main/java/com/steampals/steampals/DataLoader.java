af959d0bb508808b8311812cfb5b28b9fe431fe1
Sebastian S <74125017+sebasinmas@users.noreply.github.com>
2025-05-05 03:08:43 -0400

    Carga data en el H2

---
+++ backend/src/main/java/com/steampals/steampals/DataLoader.java
@@ -0,0 +1,29 @@
+package com.steampals.steampals;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.steampals.steampals.model.Usuario;
+import com.steampals.steampals.repository.UsuarioRepository;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (usuarioRepository.count() == 0) {
+            Usuario usuario = new Usuario();
+            usuario.setUsuario("Kevin");
+            usuario.setEmail("kevin@email.com");
+            usuarioRepository.save(usuario);
+
+            System.out.println("🟢 Usuario de prueba cargado en H2.");
+        }else {
+            throw new Exception("🔴 Base de datos ya contiene datos.");
+        }
+    }
+
+}
