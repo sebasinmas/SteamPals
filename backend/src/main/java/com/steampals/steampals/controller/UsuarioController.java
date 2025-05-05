b8e3f550c521c8580779aa342977f703c45a7e8e
Sebastian S <74125017+sebasinmas@users.noreply.github.com>
2025-05-05 02:39:58 -0400

    Update UsuarioController.java
    Cambia de controller normal a rest

--- backend/src/main/java/com/steampals/steampals/controller/UsuarioController.java
+++ backend/src/main/java/com/steampals/steampals/controller/UsuarioController.java
@@ -1,8 +1,13 @@
 package com.steampals.steampals.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+
+@RestController
 public class UsuarioController {
-    
+    @GetMapping("/usuario")
+    public String getUsuario() {
+        return "Usuario endpoint reached!";
+    }
 }
