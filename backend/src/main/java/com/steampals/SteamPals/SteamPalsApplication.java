7def139d365840e6f7a87fa4f6c210c32f82bd95
Sebastian S <74125017+sebasinmas@users.noreply.github.com>
2025-05-19 02:11:42 -0400

    breaking changes

--- backend/src/main/java/com/steampals/SteamPals/SteamPalsApplication.java
+++ backend/src/main/java/com/steampals/SteamPals/SteamPalsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SteamPalsApplication {
+public class SteampalsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SteamPalsApplication.class, args);
+		SpringApplication.run(SteampalsApplication.class, args);
 	}
 
 }
