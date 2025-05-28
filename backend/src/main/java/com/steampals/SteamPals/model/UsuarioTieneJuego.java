d32116aacaa69d6709bfd80c5ad84a91fb305de7
ElLuck15 <sebitaluck2015@gmail.com>
2025-05-28 00:01:44 -0400

    Minifixdev

--- backend/src/main/java/com/steampals/SteamPals/model/UsuarioTieneJuego.java
+++ backend/src/main/java/com/steampals/SteamPals/model/UsuarioTieneJuego.java
@@ -1,4 +1,17 @@
 package com.steampals.SteamPals.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UsuarioTieneJuego {
+    private float horasJugadas;
 }
