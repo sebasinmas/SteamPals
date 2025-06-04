e422b2833d9ca947c0236a9638b986575689c547
Sebastian S <74125017+sebasinmas@users.noreply.github.com>
2025-06-04 11:24:49 -0400

    Chore(backend): Mejora los controller

--- backend/src/main/java/com/steampals/steampals/controller/api/UsuarioController.java
+++ backend/src/main/java/com/steampals/steampals/controller/api/UsuarioController.java
@@ -2,14 +2,16 @@ package com.steampals.steampals.controller.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 public class UsuarioController {
     @GetMapping("/usuario")
     public String getUsuario() {
-        return "Usuario endpoint reached!";
+        return "Usuario get endpoint reached!";
     }
     @PutMapping("/usuario")
     public String putUsuario() {
