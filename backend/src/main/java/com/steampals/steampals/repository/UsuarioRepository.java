27dfae63922b9fe39be3384f1f9e012cb5dec788
Sebastian S <74125017+sebasinmas@users.noreply.github.com>
2025-06-16 02:27:40 -0400

    feature(backend): Registro usuario
    Cimienta los primeros pasos para poder exponer un endpoint de registro

--- backend/src/main/java/com/steampals/steampals/repository/UsuarioRepository.java
+++ backend/src/main/java/com/steampals/steampals/repository/UsuarioRepository.java
@@ -4,10 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.lang.NonNull;
 
 import com.steampals.steampals.model.Usuario;
-import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Optional<Usuario> findByNombreUsuario(String usuario);
-    Optional<Usuario> findByEmail(String email);
-    @NonNull
-    Optional<Usuario> findById(@NonNull Long id);
+    boolean existsByEmail(@NonNull String email);
+    
 }
